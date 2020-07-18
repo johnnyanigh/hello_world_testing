90a1c8f7a1a2322cd669057efe172dd43e97b412
Johnny N <johnny.a.nigh@gmail.com>
2020-07-18 11:55:29 -0500

    update: .gitignore & linter
     -Update the gitignore file to match the other projects
    
     -Use a linter on the Python files.

--- load_testing/web/locust/app.py
+++ load_testing/web/locust/app.py
@@ -2,10 +2,12 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     name = request.args.get("name", "Flask")
     return f'Hello, world from {escape(name)}!'
 
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- load_testing/web/locust/locustfile.py
+++ load_testing/web/locust/locustfile.py
@@ -1,10 +1,13 @@
 from locust import HttpLocust, TaskSet, between
 
+
 def index(l):
     l.client.get("/")
 
+
 class UserBehavior(TaskSet):
     tasks = {index: 1}
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
